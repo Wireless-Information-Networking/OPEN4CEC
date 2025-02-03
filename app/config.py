ebe2369e91f40e1d5e0add06015e8b98627c6656
Adrià Arús <102878569+Addruss@users.noreply.github.com>
2025-02-03 12:56:04 +0100

    Delete app directory

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-    REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-    WEATHER_API_API_KEY = os.getenv('WEATHER_API_API_KEY')
-    ENTSO_E_API_KEY = os.getenv('ENTSO_E_API_KEY')
